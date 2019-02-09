c7fe2df25a4956978157b4361be54c01da819a4c
Armin Vieweg <armin@v.ieweg.de>
2019-02-09 17:15:35 +0100

    [INITIAL] First commit

---
+++ README.rst
@@ -0,0 +1,10 @@
+TYPO3 CMS Extension: Save with shortcut
+=======================================
+
+Adds the possibility to use the keyboard shortcut CTRL+S to save in TYPO3 CMS backend.
+
+
+.. image:: Resources/Public/Icons/Extension.png
+   :alt: EXT:save icon
+
+
---
+++ ext_emconf.php
@@ -0,0 +1,29 @@
+<?php
+
+/*  | This extension is made with ❤ for TYPO3 CMS and zazudesign.
+ *  | It is licensed under GNU General Public License.
+ *  |
+ *  | (c) 2019 Armin Vieweg <armin@v.ieweg.de>
+ */
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Save with shortcut',
+    'description' => 'Adds the possibility to use the keyboard shortcut CTRL+S to save in TYPO3 CMS backend.',
+    'category' => 'be',
+    'version' => '1.0.0',
+    'state' => 'alpha',
+    'author' => 'Armin Vieweg',
+    'author_email' => 'armin@v.ieweg.de',
+    'author_company' => '',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0-9.5.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+    'suggests' => [],
+    'autoload' => [
+        'psr-4' => ['T3\\Save\\' => 'Classes']
+    ],
+];
